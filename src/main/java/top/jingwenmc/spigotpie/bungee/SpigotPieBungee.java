ca0108b0e700d3d1bcf1e2ff1aa8885830d93c74
jingwenMC <2313055824@qq.com>
2022-12-20 19:02:24 +0800

    初始提交

---
+++ src/main/java/top/jingwenmc/spigotpie/bungee/SpigotPieBungee.java
@@ -0,0 +1,16 @@
+package top.jingwenmc.spigotpie.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class SpigotPieBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
