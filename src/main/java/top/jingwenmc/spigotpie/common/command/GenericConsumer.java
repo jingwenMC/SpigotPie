effd13a89f909bb77ac6d7921d93621069a928ee
jingwenMC <2313055824@qq.com>
2022-12-27 22:31:05 +0800

    绕晕了

--- src/main/java/top/jingwenmc/spigotpie/common/command/GenericConsumer.java
+++ src/main/java/top/jingwenmc/spigotpie/common/command/GenericConsumer.java
@@ -4,6 +4,7 @@ import lombok.SneakyThrows;
 import net.md_5.bungee.api.ChatColor;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,13 +14,13 @@ import java.util.function.Consumer;
 public class GenericConsumer implements Consumer<CommandItem> {
     private final Object targetObject;
     private final Method targetMethod;
-    private final List<Class<?>> types;
+    private final List<Parameter> methodParameters;
     private final int leastRequired;
 
     public GenericConsumer(Object o, Method m) throws TypeErrorException {
         targetObject = o;
         targetMethod = m;
-        types = new ArrayList<>();
+        methodParameters = new ArrayList<>();
         boolean first = false;
         int leastRequired = 0;
         if(m.getParameterTypes().length == 1 && m.getParameterTypes()[0].getName().equalsIgnoreCase(SupportedTypes.COMMAND_ITEM)) {
@@ -27,21 +28,21 @@ public class GenericConsumer implements Consumer<CommandItem> {
             this.leastRequired = -1;
             return;
         }
-        for(Class<?> c : m.getParameterTypes()){
+        for(Parameter p : m.getParameters()){
             if(!first) {
-                if(!Objects.equals(c.getName(),SupportedTypes.COMMAND_SENDER)) {
-                    throw new TypeErrorException("Unrecognized 1st parameter:"+c.getName());
+                if(!Objects.equals(p.getType().getName(),SupportedTypes.COMMAND_SENDER)) {
+                    throw new TypeErrorException("Unrecognized 1st parameter:"+p.getType().getName());
                 }
                 first = true;
                 continue;
             }
-            if(!Objects.equals(c.getName(), SupportedTypes.STRING)) {
-                throw new TypeErrorException("Unrecognized type:"+c.getName());
+            if(!Objects.equals(p.getType().getName(), SupportedTypes.STRING)) {
+                throw new TypeErrorException("Unrecognized type:"+p.getType().getName());
             }
-            if(!c.isAnnotationPresent(NotRequiredCommandParam.class)) {
+            if(!p.isAnnotationPresent(NotRequiredCommandParam.class)) {
                 leastRequired++;
             }
-            types.add(c);
+            methodParameters.add(p);
         }
         this.leastRequired = leastRequired;
     }
@@ -57,20 +58,32 @@ public class GenericConsumer implements Consumer<CommandItem> {
             commandItem.getSender().sendMessage(ChatColor.RED+"指令的参数不足！至少需要"+leastRequired+"个参数！");
             //TODO:Localized message
         }
-        if(length>types.size()){
+        if(length> methodParameters.size()){
             commandItem.getSender().sendMessage(ChatColor.RED+"指令的参数过多！最多需要"+length+"个参数！");
             //TODO:Localized message
         }
         List<Object> parameters = new ArrayList<>();
         parameters.add(commandItem.getSender());
-        parameters.addAll(Arrays.asList(commandItem.getArgs()));
-        for(int i =0;i< types.size()-parameters.size()-1;i++) {
-            parameters.add(types.get(parameters.size()+i).getAnnotation(NotRequiredCommandParam.class).defaultValue());
+        if(methodParameters.size() - leastRequired != 1) {
+            parameters.addAll(Arrays.asList(commandItem.getArgs()));
+            for (int i = 0; i < methodParameters.size() - parameters.size() + 1; i++) {
+                parameters.add(methodParameters.get(parameters.size() + i).getAnnotation(NotRequiredCommandParam.class).value());
+            }
+        } else {
+            int p = 0;
+            for(Parameter pp : methodParameters) {
+                if(pp.isAnnotationPresent(NotRequiredCommandParam.class) && length == leastRequired) {
+                    parameters.add(pp.getAnnotation(NotRequiredCommandParam.class).value());
+                } else {
+                    parameters.add(commandItem.getArgs()[p]);
+                    p++;
+                }
+            }
         }
         targetMethod.invoke(targetObject,parameters.toArray());
     }
 
-    static class SupportedTypes {
+    public static class SupportedTypes {
         public static final String COMMAND_SENDER = CommandSender.class.getName();
         public static final String STRING = String.class.getName();
         public static final String COMMAND_ITEM = CommandItem.class.getName();
--- src/main/java/top/jingwenmc/spigotpie/common/command/NotRequiredCommandParam.java
+++ src/main/java/top/jingwenmc/spigotpie/common/command/NotRequiredCommandParam.java
@@ -4,10 +4,15 @@ import java.lang.annotation.*;
 
 /**
  *  Used to identify when a parameter in command is not required
+ *  WARNING: if more than one was set, please only use this annotation in the last few params!
  */
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface NotRequiredCommandParam { //只能在最后几个空添加
-    String defaultValue() default "";
+public @interface NotRequiredCommandParam { //只能在最后几个空添加.
+    /**
+     * Default value if not present
+     * @return the value
+     */
+    String value() default "";
 }
