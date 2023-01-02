0818fe5a1e028ff3fe0394f0af060e398f7e6c9b
jingwenMC <2313055824@qq.com>
2023-01-02 17:33:24 +0800

    Remove an unused class

--- src/main/java/top/jingwenmc/spigotpie/common/command/BaseCommandContainer.java
+++
@@ -1,7 +0,0 @@
-package top.jingwenmc.spigotpie.common.command;
-
-public interface BaseCommandContainer {
-    void noPermission(CommandSender sender,String... args);
-
-    void notFound(CommandSender sender,String... args);
-}
