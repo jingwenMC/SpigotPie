0a54fdab19e6971a5db5eaec2abcb60fdbbc61f6
SakuraTao <2585257181@qq.com>
2024-02-08 22:59:14 +0800

    add warning msg about @Wire couldn't match variable with @PieComponent's name

--- src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
+++ src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
@@ -1,7 +1,7 @@
 package top.jingwenmc.spigotpie;
 
 public class PieDistroConfigurations {
-    public static final String DISTRO_VERSION = "2.1.0";
+    public static final String DISTRO_VERSION = "2.1.1-alpha";
 
     public static final int API_VERSION = 3;
 }
