628c69b483c5b38aae6cd9d401125bce9f7a81d8
jingwenMC <2313055824@qq.com>
2024-02-08 23:46:31 +0800

    Change version

--- src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
+++ src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
@@ -1,7 +1,7 @@
 package top.jingwenmc.spigotpie;
 
 public class PieDistroConfigurations {
-    public static final String DISTRO_VERSION = "2.1.1-alpha";
+    public static final String DISTRO_VERSION = "2.1.1";
 
     public static final int API_VERSION = 3;
 }
