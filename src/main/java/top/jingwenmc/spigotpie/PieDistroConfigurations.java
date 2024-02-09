76ebbe793567a0ca8ad09af349b1bbc2f7fd80e7
jingwenMC <2313055824@qq.com>
2024-02-09 21:43:23 +0800

    rollback

--- src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
+++ src/main/java/top/jingwenmc/spigotpie/PieDistroConfigurations.java
@@ -3,5 +3,5 @@ package top.jingwenmc.spigotpie;
 public class PieDistroConfigurations {
     public static final String DISTRO_VERSION = "3.0.0-alpha";
 
-    public static final int API_VERSION = 5;
+    public static final int API_VERSION = 4;
 }
